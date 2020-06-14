713ca37ef46e0751a0ee9c4bfd744b87dc676b13
Samantha Wabi <swfullpotential@gmail.com>
2020-06-14 21:05:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,29 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  attr_accessor :URL 
+  
+  def initialize(url)
+    @URL = URL
+  end
+  
+  def get_response_body
+     uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  
+  def parse_json
+    results = JSON.parse(self.get_response_body)
+    results.collect do |result|
+      result["agency"]
+      get_requester = GetRequester.new(URL)
+      results = get_requester.parse_json
+    end
+  end
+  
+end
+# datum = GetRequester.new
+# puts datum.parse_json.uniq
